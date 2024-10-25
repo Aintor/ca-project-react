b669076b7ac616c4fa271cbd14cd8cb9b32013ac
Jason Wong <86539564+Aintor@users.noreply.github.com>
2024-10-25 11:40:17 +0800

    update the js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@/components': path.join(process.cwd(), 'src', 'app', 'components'),
+            '@/public/img': path.join(process.cwd(), 'public', 'img'),
+            '@@/public/favicon.ico': path.join(process.cwd(), 'public', 'favicon.ico')
+        };
+        return config;
+    },
+};
 
 export default nextConfig;
